c6499b88008f8942be8d469eb4dc0464c5d61be2
Lenadro Silva Lima <leandroslima09@gmail.com>
2025-12-03 11:00:35 +0000

    copmeço da agenda

---
+++ tasks/poo/agenda/py/draft.py
@@ -0,0 +1,24 @@
+class Fone:
+    def__init__(self,id:str, number:str):
+        self.id = id
+        self.number = number
+    
+class Contact:
+    def __init__(self, name: str):
+
+    def addFone(self,id, number):
+        f = Fone (id, number)
+
+def main():
+    agenda : Agenda()
+        while True:
+        try:
+        line = input.strip()
+            except EOFError:
+        break
+
+        if line == " ":]
+        continue
+
+
+main()
